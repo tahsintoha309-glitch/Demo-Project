44eda72e3b1a1af97f517783ddcb1359f30efda7
MrIDC <tahsintoha309@gmail.com>
2026-07-26 11:58:20 +0600

    Implemented semester course helpers

--- modules/course.c
+++ modules/course.c
@@ -1,22 +1,40 @@
-#include<stdio.h>
-#include<string.h>
-#include<course.h>
+#include <stdio.h>
+#include <string.h>
+#include "course.h"
 
-Course createCourse(char code[],char name[], double credit)
+const char null_course_code[] = "NULL";
+
+Course createCourse(char code[], char name[], double credit, int semester)
 {
     Course course;
 
-    strcpy(course.code,code);
-    strcpy(course.name,name);
-    course.credit=credit;
+    strcpy(course.code, code);
+    strcpy(course.name, name);
+    course.credit = credit;
+    course.semester = semester;
 
     return course;
 }
 
-void viewCourse(Course course)
-
+int countCoursesBeforeNull(Course courses[], int n_courses)
 {
-    printf("Code: %s\n",course.code);
-    printf("Name: %s\n",course.name);
-    printf("Credit: %.1lf\n",course.credit);
-}   
+    int count = 0;
+
+    while (count < n_courses)
+    {
+        if (strcmp(courses[count].code, null_course_code) == 0)
+            return count;
+
+        count++;
+    }
+
+    return count;
+}
+
+void viewCourse(Course course)
+{
+    printf("Code: %s\n", course.code);
+    printf("Name: %s\n", course.name);
+    printf("Credit: %.1f\n", course.credit);
+    printf("Semester: %d\n", course.semester);
+} 
