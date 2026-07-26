d16a9c916dc400daade747b968377e07da2729c8
MrIDC <tahsintoha309@gmail.com>
2026-07-26 11:52:34 +0600

    Declared semester course helpers

--- modules/courseResult.h
+++ modules/courseResult.h
@@ -10,8 +10,11 @@ typedef struct CourseResult
     int completed;
 } CourseResult;
 
-CourseResult createCompletedCourseResult(Course *course, double marks);
-CourseResult createIncompleteCourseResult(Course *course);
+CourseResult createCourseResult(Course *course, double marks);
+void sortCourseResultsBySemester(CourseResult results[], int n_results);
+void filterCourseResultsBySemester(CourseResult results[], int n_results, int semester,
+                                   CourseResult filtered[]);
+int countCourseResultsBeforeNull(CourseResult results[], int n_results);
 void viewCourseResult(CourseResult result);
 
 #endif
